7d2253cdb5d4d7a0bb9c4d8ac583f1a55ac2a021
StephLin <stephen359595@gmail.com>
2021-09-19 14:20:03 +0800

    💥 Decoupled post-processing and calibration

--- det3d/models/bbox_heads/__init__.py
+++ det3d/models/bbox_heads/__init__.py
@@ -4,7 +4,6 @@
 # mg_head_ciassd: !!! Faster version of CIA-SSD !!!
 # mg_head_sessd: work with mean teacher framework. !!! Work and Fixed !!! The cls_thres may be modified.
 
-
-from .mg_head_sessd import MultiGroupHead
+from .mg_head_sessd_nocalib import MultiGroupHead
 
 __all__ = ["MultiGroupHead"]
